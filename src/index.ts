49824a0177233031da719b1f598cbbd908ec0881
nowaaru@proton.me <vert@leanbox.>
2024-09-01 10:54:42 +0000

    exports and remove ugly, unnecessary imports
    Signed-off-by: nowaaru@proton.me <vert@leanbox.>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 import type Flamework from "@flamework/core";
 import { Modding, type Modding as FWModding } from "@flamework/core";
 
-import { CharacterSelectController } from "controllers/characterselect.controller"
-import { Gamepad } from "controllers/gamepad.controller"
-import { Input } from "controllers/input.controller"
-import { Keyboard } from "controllers/keyboard.controller"
-import { MatchController } from "controllers/match.controller"
-import { Mouse } from "controllers/mouse.controller"
-import { ResourceController } from "controllers/resourcecontroller.controller"
-
-export * from "controllers/client.controller";
 export * from "controllers/input.controller";
 export * from "controllers/mouse.controller";
 export * from "controllers/keyboard.controller";
@@ -33,10 +24,10 @@ export * from "module/character";
 export * from "module/combat";
 
 export * from "module/game/client";
+
 export * from "lib/player";
 
 export * from "module/extra/cursor";
-export * from "module/extra/cmdr";
 export * from "module/extra/hud";
 
 export * from "network";
