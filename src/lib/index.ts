3bddbb08b86a384cc4becc67fb03064991072dc4
nowaaru@proton.me <vert@leanbox.>
2024-09-10 09:54:41 +0000

    integrate with new common package
    Signed-off-by: nowaaru@proton.me <vert@leanbox.>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
-import { QuarrelFunctions } from "@quarrelgame-framework/common";
+import { Dependency } from "@flamework/core";
+import { QuarrelFunctions, SchedulerService } from "@quarrelgame-framework/common";
 
 /**
  * Get the tick rate.
  */
 export const GetTickRate =
-    () => (QuarrelFunctions.createClient({}).GetGameTickRate().await()[1] as number | undefined);
+    () => Dependency<SchedulerService>().GetTickRate()
